55da91beaaccc43c186c6642bc5888e7605e3916
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-24 16:18:45 +0400

    Update sample

--- CS/DXSample_FocusingCells/Window1.xaml.cs
+++ CS/DXSample_FocusingCells/Window1.xaml.cs
@@ -1,36 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using DevExpress.Xpf.Grid;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using DevExpress.Xpf.Grid;
 
 namespace DXSample_FocusingCells {
     public partial class Window1 : Window {
         public Window1() {
             InitializeComponent();
-            grid.ItemsSource = new nwindDataSetTableAdapters.ProductsTableAdapter().GetData();
+            grid.ItemsSource = ProductsData.GetProducts();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            if (grid.VisibleRowCount == 0) return;
-            int rowHandle = view.FocusedRowHandle + 1;
-            while (Convert.ToDouble(grid.GetCellValue(rowHandle, "UnitPrice")) != 10 && 
-                grid.IsValidRowHandle(rowHandle)) {
-                    rowHandle++;
-            }
-            if (grid.IsValidRowHandle(rowHandle)) {
-                grid.CurrentColumn = grid.Columns["UnitPrice"];
-                view.FocusedRowHandle = rowHandle;
-            }
+        void FocusCell(object sender, RoutedEventArgs e) {
+            if (!double.TryParse(textBox.Text, out double textValue))
+                return;
+            var rowHandle = grid.FindRowByValue(grid.Columns[nameof(Product.UnitPrice)], textValue);
+            if (rowHandle == DataControlBase.InvalidRowHandle) 
+                return;
+            grid.CurrentColumn = grid.Columns[nameof(Product.UnitPrice)];
+            view.FocusedRowHandle = rowHandle;
         }
     }
 }
